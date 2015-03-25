415128b39856f28df20fa60fc36b60be1b462caf
Eric Newcombe <dukenewcombe95@gmail.com>
2015-03-25 14:47:22 -0400

    Dynamically creates a gameboard. Given a width, height and number of mines it will generate a new map every time. It will now also update touching cells with the number of mines they are in contact with

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Minesweeper
+
+A simple miesweeper game to work on recursive techniques and practice what I have learned in class
--- main.js
+++ main.js
@@ -10,25 +10,85 @@ function generateGameBoard (width, height) {
 		}
 	}
 
-	return map;
+	return {width: width, height: height, map: map};
 
 }
 
-function generateMineLocations(width, height, numMines){
+function generateMines(board, numMines){
+	//Generates n mine locations on the gameboard provided and updates the cells touching the mine
+
 	var mineLocations = [];
 
 	for (var i = 0; i < numMines; i++) {
-		var potentialMine = {
-			x = Math.floor(Math.random() * width;
-			y = Math.floor(Math.random() * height;
+
+		//Generate initial point to insert into array of mine locations every time it loops
+		var xPos = Math.floor(Math.random() * board.width);
+		var yPos = Math.floor(Math.random() * board.height);
+
+		while( board.map[xPos][yPos] == 'X' ){
+			//If there is an overlap of the potential mine location, then keep recreating it until it is in a unique location
+			xPos = Math.floor(Math.random() * board.width);
+			yPos = Math.floor(Math.random() * board.height);
+			console.log("duplicate detected");
 		}
-		mineLocations.push(potentialMine);
+
+		board.map[xPos][yPos] = 'X';
+		incrementCellsTouching(board, xPos, yPos)
+
 	}
 
-	return mineLocations;
 }
 
-var map = generateGameBoard(10,10);
-var mineLocations = generateMineLocations(10,10,15);
+function appendGameBoardToScreen(gameBoard){
+	// Displays the object representing the game board onto the screen
 
-console.log(mineLocations);
+	var container = document.getElementById('playArea');
+	var board = createDiv('gameBoard', '');
+
+	for(var i = 0; i < gameBoard.height; i++){
+		var row = createDiv('row', "");
+		for (var j = 0; j < gameBoard.width; j++) {
+			var cell = createDiv('cell', gameBoard.map[i][j], function(){console.log('asdf')});
+			row.appendChild(cell);
+		}
+		board.appendChild(row);
+	}
+
+	container.appendChild(board);
+
+}
+
+function createDiv(className, innerHTML, functionOnClick){
+	var element = document.createElement('div');
+	element.className = className;
+	element.addEventListener(functionOnClick);
+	element.innerHTML = innerHTML;
+	return element;
+}
+
+function incrementCellsTouching(board, mineX, mineY){
+	for(var xOffset = -1; xOffset <= 1; xOffset++){
+		for(var yOffset = -1; yOffset <= 1; yOffset++){
+			var x = mineX + xOffset,
+				y = mineY + yOffset;
+			if(  x < 0 || x >= board.width || y < 0 || y >= board.height || board.map[x][y] == 'X' ){
+				// If the location of the x or y points that are being examined are out of the scope, or if that point is a mine continue through the loop
+				continue;
+			}
+			else {
+				board.map[x][y]++;
+			}
+		}
+	}
+}
+
+var board = generateGameBoard(10,10);
+generateMines(board,10);
+
+console.log(board);
+
+appendGameBoardToScreen(board);
+
+
+
+
